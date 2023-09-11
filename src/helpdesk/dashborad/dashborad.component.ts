ba5ee384e613816c6e133f92d6419dcec3071320
amit-khatke <141109542+amit-khatke@users.noreply.github.com>
2023-09-11 22:00:08 +0530

    3 files modified, 1 added

--- src/helpdesk/dashborad/dashborad.component.ts
+++ src/helpdesk/dashborad/dashborad.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CallLogComponent } from './call-log/call-log.component';
 import { LeftComponent } from './left/left.component';
 import { SentimentComponent } from './sentiment/sentiment.component';
+import { Information } from './information';
 
 @Component({
   standalone:true,
@@ -10,11 +11,12 @@ import { SentimentComponent } from './sentiment/sentiment.component';
   styleUrls: ['./dashborad.component.css'],
   imports: [ LeftComponent,SentimentComponent,CallLogComponent],
 })
-export class DashboradComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class DashboradComponent { 
+    inforamtion: Information = {
+      name: 'amit',
+      gender:'male',
+      dob:2/11/2003,
+      phone:'8080613413'
+    };
   }
 
-}
---
+++ src/helpdesk/dashborad/information.ts
@@ -0,0 +1,8 @@
+import { EmailValidator } from "@angular/forms";
+
+export interface Information {
+  name:string,
+  gender:string,
+  dob:any,
+  phone:string,
+}
